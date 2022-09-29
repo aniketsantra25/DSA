fbd24dc965edde7aa44acf18a2e3de2bc9a9f0c1
Aniket Santra <aniketsantra05@gmail.com>
2022-09-30 00:52:26 +0530

    First commit

---
+++ DSA/Find Safe Position (Josephus Problem).cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int safePos(int n, int k) {
+        // code here
+        if(n==1)
+            return 1;
+        return (safePos(n-1,k) + k - 1)%n + 1;
+    }
+};
