336d0a12f7e9e0a10cd84d467634ded40b88e840
Aniket Santra <aniketsantra05@gmail.com>
2022-10-02 10:23:19 +0530

    First Commit

---
+++ DSA/First Bad Version.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+    int high = n;
+    while (low < high) {
+      const int mid = low + (high - low) / 2;
+      if (isBadVersion(mid)) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return high;
+    }
+};
