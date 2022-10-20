a382d946fee51faa2ab3e183fad1fa13a12eb9cc
Aniket Santra <73291928+aniketsantra25@users.noreply.github.com>
2022-10-20 15:30:45 +0530

    Time: 109 ms (5.36%), Space: 6 MB (31.27%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n < 1){
+            return false;
+        }
+        cout<<n<<endl;
+        if(n==1 ){
+            return true;
+        }
+        if(n%3 == 0){
+            return isPowerOfThree(n/3);
+        }
+        else{
+            return false;
+        }
+    }
+};
